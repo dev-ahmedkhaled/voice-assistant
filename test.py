421aa7b071fd4586f60e404b25dc56082b6c5b35
ahmed khaled <155910005+ahmed706050@users.noreply.github.com>
2024-10-04 21:39:02 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-index = 0
-for voice in voices:
-   print(f'index-> {index} -- {voice.name}')
-   index +=1
-engine.runAndWait()
